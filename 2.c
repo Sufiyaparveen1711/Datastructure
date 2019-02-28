e55109aea98f6935dc7b78ca839524c642a68495
Sufiyaparveen1711 <43900518+Sufiyaparveen1711@users.noreply.github.com>
2019-02-28 21:43:06 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,91 @@
+//C program for insertion of a node at beginning of singly linked list.
+#include<stdio.h>
+#include<stdlib.h>
+
+void create();
+void display();
+void inst_beg();
+
+struct node
+{
+    int info;
+    struct node *link;
+};
+struct node *start=NULL;
+
+int main()
+{
+    create();
+    display();
+    inst_beg();
+    display();
+    return 0;
+}
+
+void create()
+{
+    struct node *tmp,*ptr;
+    int item;
+    char ch='y';
+    while(ch=='y'||ch=='Y')
+    {
+        tmp=(struct node*)malloc(sizeof(struct node));
+        if(tmp==NULL)
+        {
+            printf("Memory overflow.");
+            exit(0);
+        }
+        printf("Enter info value::");
+        scanf("%d",&item);
+        tmp->info=item;
+        tmp->link=NULL;
+        if(start==NULL)
+            start=tmp;
+        else
+        {
+            ptr=start;
+            while(ptr->link!=NULL)
+                ptr=ptr->link;
+            ptr->link=tmp;
+        }
+        fflush(stdin);
+        printf("Do u want to add more nodes::");
+        scanf("%c",&ch);
+    }
+}
+
+void display()
+{
+    struct node *ptr;
+    if(start==NULL)
+    {
+        printf("List is empty.");
+        exit(0);
+    }
+    else
+    {
+        ptr=start;
+        while(ptr!=NULL)
+        {
+            printf("%d\t",ptr->info);
+            ptr=ptr->link;
+        }
+    }
+}
+
+void inst_beg()
+{
+    struct node *tmp;
+    int item;
+    tmp=(struct node*)malloc(sizeof(struct node));
+    if(tmp==NULL)
+    {
+        printf("Memory overflow.");
+        exit(0);
+    }
+    printf("\nEnter info value u want to insert at beginning::");
+    scanf("%d",&item);
+    tmp->info=item;
+    tmp->link=start;
+    start=tmp;
+}
