5fb6e35fb91d21f3cc507b78db0633bad6493cb2
Sufiyaparveen1711 <43900518+Sufiyaparveen1711@users.noreply.github.com>
2019-03-02 02:04:20 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,4 +1,4 @@
-//C program for insertion of a node after given location of singly linked list.
+//C program for insertion of a node after given location of single linked list.
 #include<stdio.h>
 #include<stdlib.h>
 
