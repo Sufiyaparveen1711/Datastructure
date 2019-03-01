88b60b8eeb135a993ce8158c12fb4c73f1c5fbd9
Sufiyaparveen1711 <43900518+Sufiyaparveen1711@users.noreply.github.com>
2019-03-02 02:05:15 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,4 +1,4 @@
-//C program for insertion of a node at the beginning of singly linked list.
+//C program for insertion of a node at the beginning of single linked list.
 #include<stdio.h>
 #include<stdlib.h>
 
